b4006e7bc25bc763aaa0dc89e92607bf0854a576
Parker Brandt <parker.brandt101@gmail.com>
2022-01-26 17:54:34 -0600

    initial version of a CSV parsing test

--- src/tests/LetterGameTest.java
+++ src/tests/LetterGameTest.java
@@ -1,11 +1,26 @@
 package tests;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import org.junit.Assert.*;
 
 public class LetterGameTest {
 
     @Test
-    public void testCSVParsing() {
-        
+    public void testCSVParsing(String filename) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line = "";
+
+            while((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+
+                assertTrue(data[0].charAt(0) == data[1].charAt(1));
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
