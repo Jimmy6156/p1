8f3777e39826b8f662fc8c6fe53bc5bdbd3fa388
Jimmy Nguyen <59070730+Jimmy6156@users.noreply.github.com>
2022-01-27 13:26:36 -0600

    Update LetterGameTest.java

--- src/tests/LetterGameTest.java
+++ src/tests/LetterGameTest.java
@@ -1,6 +1,7 @@
 package p1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Random;
@@ -13,26 +14,59 @@ import org.junit.Assert.*;
 import org.junit.Test;
 public class LetterGameTest {
 	Scanner scanner = new Scanner(System.in);
-    String input = "";
+	String input = null;
     Random random = new Random();
     int randIndex = random.nextInt(26);
-
-    ArrayList<Character> englishList = new ArrayList<>();
-    ArrayList<Character> spanishList = new ArrayList<>();
-    input = scanner.next();
-    @Test
-    public void testCSVParsing() {
-       //ArrayList<Character> englishList = new ArrayList<>();
-       //ArrayList<Character> spanishList = new ArrayList<>();
-      // input = scanner.next();
-       assertEquals(englishList.get(randIndex), input.charAt(0));
-       assertEquals(spanishList.get(randIndex), input.charAt(0));
+    int max = 26;
+    private static ArrayList<Character> englishList = new ArrayList<>();
+    private static ArrayList<Character> spanishList = new ArrayList<>();
+ 
+    private static void initLists(String filename) throws IOException {
+        // Variables
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = "";
+        
+        while((line = reader.readLine()) != null) {
+            String[] data = line.split(",");
+            
+            englishList.add(data[0].charAt(0));
+            spanishList.add(data[1].charAt(0));
+        }
+        reader.close();
     }
-
-}
-
-@Test
-public void testFail() {
-
-
+    
+    @Test
+    public void testCSVParsing() { // Tests one random letter
+    	try {
+            initLists("C:/Users/jimmy/p1-1/src/main/resources/letters.csv");
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+   
+       assertTrue(englishList.get(randIndex) == spanishList.get(randIndex));
+       assertFalse(englishList.get(randIndex) != spanishList.get(randIndex));
+    	   
+    }
+    
+    public void zeroTest() {
+    	
+    }
+    
+    @Test
+    public void fullTest() { // Tests all the alphabets
+    	try {
+            initLists("C:/Users/jimmy/p1-1/src/main/resources/letters.csv");
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    	int intTest = 0;
+    	while (intTest < max) {
+    		assertTrue(englishList.get(intTest) == spanishList.get(intTest));
+    		assertFalse(englishList.get(intTest) != spanishList.get(intTest));
+    		intTest++;
+    	}
+    }
+    
+    // single, full set and zero set for testing
+    
 }
